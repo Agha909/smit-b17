c19b192794279467160e98774c11506bff31f949
Agha909 <agharafay123@gmail.com>
2025-07-31 19:34:53 +0500

    chp2,chp3 js

---
+++ JavaScript/chapter2/app.js
@@ -0,0 +1,40 @@
+// Agha Abdul Raafay
+
+//Q1
+var username;
+
+//Q2
+var myName = "Agha Abdul Raafay Muhammad";
+
+//Q3
+var message;
+message = "Hello World";
+window.alert(message);
+
+//Q4
+var studentName = "Agha";
+var studentAge = 22;
+var skill = "Certified Database Administrator";
+
+window.alert(studentName);
+window.alert(studentAge);
+window.alert(skill);
+
+//Q5
+var a = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+window.alert(a);
+
+//Q6
+var email = "agharafay123@gmail.com";
+window.alert("My email address is " + email);
+
+//Q7
+var book = "A smarter way to learn JavaScript";
+window.alert("I am trying to learn from the Book " + book);
+
+//Q8
+document.write("Yah! I can write HTML content through JavaScript");
+
+//Q9 (cant find the symbols so made a string myself)
+var design = "---------211<>112------";
+window.alert(design);
---
+++ JavaScript/chapter3/app.js
@@ -0,0 +1,22 @@
+// Agha Abdul Raafay
+
+//Q1
+var age = 22;
+window.alert(age);
+
+//Q2
+var visits = 17;
+window.alert("You have visited this site " + visits + " times");
+
+//Q3
+var birthYear = 2003;
+
+document.write("My birth year is " + birthYear + "<br>");
+document.write("Data type of my declared variable is number");
+
+//Q4
+var visitorName = "Agha";
+var productTitle = "T-shirt";
+var quantity = 4;
+
+document.write("<br>" + visitorName + " ordered " + quantity + " " + productTitle +"s on XYZ Clothing Store");
